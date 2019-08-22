e805deaf8ff68b36d5db2744222eca43973fb64b
Alex Miller <miller.alex.77@gmail.com>
2019-08-22 01:38:49 -0400

    autofocus

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ console.log("Hello!")
 
 $(document).ready(function() {
 
+  $('#newModal').on('shown.bs.modal', function () {
+    $('#modalName').trigger('focus')
+})
+
   $('#new-thing').click(function() {
     zip = $('#inlineFormInput').val()
     $('#modalBoom').val(zip)
