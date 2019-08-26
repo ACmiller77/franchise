ea472ae0f3e2459028f64e11b0acd7b35fb13ed8
Alex Miller <miller.alex.77@gmail.com>
2019-08-26 16:12:00 -0400

    added code to make forms submit data

--- script.js
+++ script.js
@@ -16,4 +16,24 @@ $(document).ready(function() {
     console.log("submitted");
   })
 
+  $('#form-quote').submit(function(e) {
+    e.preventDefault();
+    formSubmit(e.currentTarget.name);
+  });
+
+  function formSubmit(name) {
+  var serializedData = $(document.forms[name]).serializeObject();
+  var url = 'https://script.google.com/macros/s/AKfycbzHZbXwOD88U7mbeYhpyPULIuT2agoTq8QfPMYBP-Kb4mJA-TI/exec'; // get this url
+
+  var jqxhr = $.ajax({
+    url,
+    method: "GET",
+    dataType: "json",
+    data: serializedData,
+    success: function(data){
+      console.log('success', data);
+    }
+  });
+}
+
 })
